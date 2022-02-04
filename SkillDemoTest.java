58b0332bbfbfe5fe27ea6fa246085c6f09983963
wross3150 <81711875+wross3150@users.noreply.github.com>
2022-02-04 12:07:57 -0800

    fixing the test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.divide(2, 2));
+        assertEquals(2, SkillDemo.divide(2, 1));
     }
 }
