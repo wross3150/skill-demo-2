54c7fd8fe23d1448ce73fb8271b25229662081d8
wross3150 <81711875+wross3150@users.noreply.github.com>
2022-02-04 12:05:15 -0800

    Starting Failed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.divide(2, 2));
+    }
+}
